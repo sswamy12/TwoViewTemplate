c8ed9db3c8503ed32799f44ba5b08f9445dd30c0
Skanda Swamy <skanda_swamy@hotmail.com>
2019-01-12 07:08:06 -0800

    Initial Commit

---
+++ TwoViewTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoViewTemplate
+//
+//  Created by Skanda Swamy on 1/12/19.
+//  Copyright © 2019 Hiruco.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
